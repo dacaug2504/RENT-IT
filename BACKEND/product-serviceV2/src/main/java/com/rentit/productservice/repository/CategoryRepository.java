1ad347e3c40f9351f4e11d675fdf35fec8b53b68
Sawant Avishkar M. <144624560+BugBite003@users.noreply.github.com>
2026-01-28 11:58:44 +0530

    Delete BACKEND directory

--- BACKEND/product-serviceV2/src/main/java/com/rentit/productservice/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rentit.productservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.rentit.productservice.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    // no methods needed now
-}
--- BACKEND/product-serviceV2/src/main/java/com/rentit/productservice/repository/OwnerItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rentit.productservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.rentit.productservice.entity.OwnerItem;
-
-public interface OwnerItemRepository extends JpaRepository<OwnerItem, Integer> {
-}
